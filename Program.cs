54a13a94f92df6e82d256f60fc607c85611b6a96
EudesNunes <araujonuneseudes@gmail.com>
2022-12-06 13:58:02 -0300

    inicial

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(500);
+            Console.WriteLine("Set..");
+            Thread.Sleep(500);
+            Console.WriteLine("Go!");
+            Thread.Sleep(500);
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = time;
+            while (currentTime >= 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"Tempo: {currentTime}/{time}");
+                currentTime--;
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Cronometro Finalizado!");
+            Thread.Sleep(1000);
+            Menu();
+        }
+    }
+}
